6a41402ab3ad801eafee2756f88aefa1c98491f7
PGallagher93 <Pgallagher93@hotmail.com>
2023-08-14 16:36:45 +0100

    completes the recommended changes to task 3

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -1,14 +1,9 @@
-const fs = require("fs/promises");
+
+const endpointData = require("../endpoints.json")
 
 exports.getEndpoints = (request, response, next) =>{
-    fs.readFile("./endpoints.json", "utf-8").then((data)=>{
-        const endpoints = JSON.parse(data)
-        
-        
-        
-        return response.status(200).send(endpoints)
-        
-    }).catch((err) =>{
-        next(err)
-    })
+    
+
+    response.status(200).send(endpointData)
+    
 }
